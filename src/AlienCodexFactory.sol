c06e8f19aae4d9b1ebd1b13f9b1d11acccb23cd5
0x5chn0uf <0x5chn0uf@proton.me>
2024-06-29 13:36:51 +0200

    feat: Added all levels

---
+++ src/AlienCodexFactory.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./base/Level-05.sol";
+import "./AlienCodex.sol";
+
+contract AlienCodexFactory is Level {
+    function createInstance(
+        address _player
+    ) public payable override returns (address) {
+        _player;
+        return address(new AlienCodex());
+    }
+
+    function validateInstance(
+        address payable _instance,
+        address _player
+    ) public override returns (bool) {
+        // _player;
+        AlienCodex instance = AlienCodex(_instance);
+        return instance.owner() == _player;
+    }
+}
